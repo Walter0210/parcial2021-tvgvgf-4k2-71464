788ed5f217a532e80df92db1062b4b6ebd97f0a8
Walter0210 <walter.ebalderrama@gmail.com>
2021-06-29 12:46:43 -0300

    2 files modified

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from '../../models/producto';
 import { ProductosServiceService } from '../../services/productos.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalDialogService } from '../../services/modal-dialog.service';
 
 @Component({
   selector: 'app-productos',
@@ -8,7 +10,7 @@ import { ProductosServiceService } from '../../services/productos.service';
   styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent implements OnInit {
-  Titulo = 'Producots';
+  Titulo = 'Productos';
   TituloAccionABMC = {
     A: '(Agregar)',
     B: '(Eliminar)',
@@ -19,17 +21,46 @@ export class ProductosComponent implements OnInit {
   AccionABMC = 'L';
   Items: Producto[] = null;
   submitted: boolean = false;
+  FormAlta: FormGroup;
 
-  constructor(private productoService: ProductosServiceService) {}
+  constructor(
+    private productoService: ProductosServiceService,
+    private modalDialogService: ModalDialogService,
+    public formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.FormAlta = this.formBuilder.group({
+      ProductoID: [0],
+      ProductoNombre: [
+        null,
+        [Validators.required, Validators.minLength(5), Validators.maxLength(50)]
+      ],
+      ProductoStock: [
+        null,
+        [Validators.required, Validators.pattern('^\\d{1,10}$')]
+      ],
+      ProductoFechaAlta: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(
+            '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+          )
+        ]
+      ]
+    });
+
     this.GetProductos();
   }
-
 
   GetProductos() {
     this.productoService.get().subscribe((res: Producto[]) => {
       this.Items = res;
     });
   }
+
+  Grabar() {}
+  Volver() {}
+  Agregar() {}
 }
